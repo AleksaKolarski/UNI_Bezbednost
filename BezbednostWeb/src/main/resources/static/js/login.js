d82927752731c290aced9f707c7f43ea41c60107
Aleksa-PC <aleksa.kolarski@yahoo.com>
2019-05-18 22:17:59 +0200

    W login page done

---
+++ BezbednostWeb/src/main/resources/static/js/login.js
@@ -0,0 +1,38 @@
+
+var input_email;
+var input_password;
+var button_login;
+
+$(document).ready(function(e){
+  
+  input_email = $('#id-input-email');
+  input_password = $('#id-input-password');
+  button_login = $('#id-button-login');
+
+  add_validation_email(input_email, 30);
+  add_validation_text(input_password, 5, 999);
+
+  button_login.on('click', function(e){
+    var email = input_email.val();
+    var password = input_password.val();
+
+    if(check_email(input_email, 30)){
+      if(check_text(input_password, 5, 999)){
+        customAjax({
+          method: 'POST',
+          url: 'auth/login',
+          data: { 'email': email, 'password': password },
+          success: function(jwt, status, xhr){
+            if(xhr.status == 200){
+              localStorage.setItem('jwt', jwt);
+              window.location.href = "/index.html";
+            }
+          },
+          error: function(xhr, status, error){
+
+          }
+        });
+      }
+    }
+  });
+});
